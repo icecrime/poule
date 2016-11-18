5d82d1684c4b21eb1a96e962c65befa51885631c
Arnaud Porterie (icecrime) <arnaud.porterie@docker.com>
2016-11-17 21:59:42 -0800

    Serve: add support for `status` live event
    In `serve` mode, handle `status` events by looking for all pull requests
    for that repository which contain the specified SHA. This allows to hook
    the event and react when a pull request state changes.
    
    Signed-off-by: Arnaud Porterie (icecrime) <arnaud.porterie@docker.com>

--- src/poule/server/handler.go
+++ src/poule/server/handler.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"strings"
 
 	"poule/configuration"
 	"poule/gh"
@@ -28,24 +29,31 @@ func (s *Server) HandleMessage(message *nsq.Message) error {
 		return err
 	}
 
-	// Parse into a GitHub in order to extract the repository information.
-	item, err := makeGitHubItem(m.GitHubEvent, message.Body)
+	// Parse into GitHub items in order to extract the repository information.
+	items, err := makeGitHubItems(&s.config.Config, m.GitHubEvent, message.Body)
 	switch {
 	case err != nil:
 		return err
-	case item == nil:
+	case len(items) == 0:
 		return nil
 	}
 
-	// Avoid logging status event which are just too frequent and noisy.
-	if m.GitHubEvent != "status" {
-		logrus.WithFields(logrus.Fields{
-			"action":     m.Action,
-			"event":      m.GitHubEvent,
-			"number":     item.Number(),
-			"repository": item.Repository(),
-		}).Debugf("received GitHub event")
+	// Handle the event for every GitHub item related to this event.
+	for _, item := range items {
+		if err := s.handleMessageForItem(m, item); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func (s *Server) handleMessageForItem(m partialMessage, item gh.Item) error {
+	logrus.WithFields(logrus.Fields{
+		"action":     m.Action,
+		"event":      m.GitHubEvent,
+		"number":     item.Number(),
+		"repository": item.Repository(),
+	}).Debugf("received GitHub event")
 
 	// Gather the list of potential actions for that repository.
 	actions := s.config.CommonActions
@@ -58,7 +66,7 @@ func (s *Server) HandleMessage(message *nsq.Message) error {
 outer_loop:
 	for _, actionConfig := range actions {
 		if actionConfig.Triggers.Contains(m.GitHubEvent, m.Action) {
-			if err := s.executeAction(actionConfig, *item); err != nil {
+			if err := s.executeAction(actionConfig, item); err != nil {
 				return err
 			}
 			continue outer_loop
@@ -89,26 +97,72 @@ func (s *Server) executeAction(action configuration.Action, item gh.Item) error 
 	return nil
 }
 
-func makeGitHubItem(event string, data []byte) (*gh.Item, error) {
+func makeGitHubItems(c *configuration.Config, event string, data []byte) ([]gh.Item, error) {
 	switch event {
 	case "issues", "issue_comment":
-		var evt *github.IssuesEvent
-		if err := json.Unmarshal(data, &evt); err != nil {
-			return nil, err
-		}
-		// Yet another quirk of the GitHub API: the "repository" field inside
-		// the issue object is nil, but not at the event root.
-		evt.Issue.Repository = evt.Repo
-		item := gh.MakeIssueItem(evt.Issue)
-		return &item, nil
+		return makeItemsFromIssueEvent(c, data)
 	case "pull_request", "pull_request_review", "pull_request_review_comment":
-		var evt *github.PullRequestEvent
-		if err := json.Unmarshal(data, &evt); err != nil {
-			return nil, err
-		}
-		item := gh.MakePullRequestItem(evt.PullRequest)
-		return &item, nil
+		return makeItemsFromPullRequestEvent(c, data)
+	case "status":
+		return makeItemsFromStatusEvent(c, data)
 	default:
 		return nil, nil
 	}
+}
+
+func makeItemsFromIssueEvent(c *configuration.Config, data []byte) ([]gh.Item, error) {
+	var evt *github.IssuesEvent
+	if err := json.Unmarshal(data, &evt); err != nil {
+		return []gh.Item{}, err
+	}
+
+	// Yet another quirk of the GitHub API: the "repository" field inside
+	// the issue object is nil, but not at the event root.
+	evt.Issue.Repository = evt.Repo
+	item := gh.MakeIssueItem(evt.Issue)
+	return []gh.Item{item}, nil
+}
+
+func makeItemsFromPullRequestEvent(c *configuration.Config, data []byte) ([]gh.Item, error) {
+	var evt *github.PullRequestEvent
+	if err := json.Unmarshal(data, &evt); err != nil {
+		return []gh.Item{}, err
+	}
+
+	item := gh.MakePullRequestItem(evt.PullRequest)
+	return []gh.Item{item}, nil
+}
+
+func makeItemsFromStatusEvent(c *configuration.Config, data []byte) ([]gh.Item, error) {
+	var evt *github.StatusEvent
+	if err := json.Unmarshal(data, &evt); err != nil {
+		return []gh.Item{}, err
+	}
+
+	// Search for all pull request that match this commit SHA. Note that it's perfectly fine for a
+	// single commit to belong to multiple pull requests (example: when a patch was cherry-picked in
+	// multiple places).
+	client := gh.MakeClient(c)
+	result, _, err := client.Search().Issues(*evt.SHA, nil)
+	if err != nil {
+		return []gh.Item{}, err
+	}
+	logrus.Debugf("found %d matching items for SHA %s", *result.Total, *evt.SHA)
+
+	// TODO Retrieve the commit list for the pull request, and verify that the SHA is indeed part of
+	// the pull requests commits. This avoids matching pull request that contain the specified SHA
+	// as part of their title or body.
+	pulls := []gh.Item{}
+	for _, issue := range result.Issues {
+		// The issue object has an empty repository information, so we need to extract if from the
+		// issue's HTML URL... <insert crying emoji here>
+		if strings.HasPrefix(*issue.HTMLURL, "https://github.com/"+*evt.Repo.FullName) {
+			pull, _, err := client.PullRequests().Get(*evt.Repo.Owner.Login, *evt.Repo.Name, *issue.Number)
+			if err != nil {
+				return []gh.Item{}, err
+			}
+			pulls = append(pulls, gh.MakePullRequestItem(pull))
+		}
+	}
+	return pulls, nil
 }
